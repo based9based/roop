0924e7e3f75772835cac9f62968f858892a53db9
based9based <142926448+based9based@users.noreply.github.com>
2023-08-23 17:32:13 +0530

    Update face_swapper.py

--- roop/processors/frame/face_swapper.py
+++ roop/processors/frame/face_swapper.py
@@ -20,14 +20,14 @@ def get_face_swapper() -> Any:
 
     with THREAD_LOCK:
         if FACE_SWAPPER is None:
-            model_path = resolve_relative_path('../models/inswapper_128.onnx')
+            model_path = resolve_relative_path('../inswapper_128.onnx')
             FACE_SWAPPER = insightface.model_zoo.get_model(model_path, providers=roop.globals.execution_providers)
     return FACE_SWAPPER
 
 
 def pre_check() -> bool:
-    download_directory_path = resolve_relative_path('../models')
-    conditional_download(download_directory_path, ['https://huggingface.co/henryruhs/roop/resolve/main/inswapper_128.onnx'])
+    download_directory_path = resolve_relative_path('../content/roop')
+    conditional_download(download_directory_path, ['https://huggingface.co/countfloyd/deepfake/resolve/main/inswapper_128.onnx'])
     return True
 
 
